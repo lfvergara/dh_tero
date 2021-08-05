fbc68fbfc05d85a8b5fb2b414798ab30bab05179
Federico Vergara <hu.ce.ro@gmail.com>
2021-08-05 20:01:37 -0300

    Cargo Áreas de Interés

--- modules/mantenimientopreventivo/controller.php
+++ modules/mantenimientopreventivo/controller.php
@@ -11,6 +11,7 @@ require_once "modules/distcetnis/model.php";
 require_once "modules/distribuidor/model.php";
 require_once "modules/ceta/model.php";
 require_once "modules/agenda/model.php";
+require_once "modules/areainteres/model.php";
 
 
 class MantenimientoPreventivoController {
