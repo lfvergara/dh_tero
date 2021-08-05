c0e403337bca42b777bd9a2c06f598b994a8018f
Federico Vergara <hu.ce.ro@gmail.com>
2021-08-05 20:09:01 -0300

    Cargo Áreas de Interés

--- modules/areainteres/model.php
+++ modules/areainteres/model.php
@@ -7,5 +7,10 @@ class AreaInteres extends StandardObject {
 		$this->areainteres_id = 0;
 		$this->denominacion = '';
 	}
+
+	function actualizar1() {
+		$sql = "update areainteres set denominacion = 'Administración' where areainteres_id = 1";
+		execute_query($sql);
+	}
 }
 ?>
