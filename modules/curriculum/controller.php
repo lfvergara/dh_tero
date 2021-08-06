d4a50585beec91bd5788fbd84367f957fe75d266
Federico Vergara <hu.ce.ro@gmail.com>
2021-08-05 23:07:18 -0300

    Agrego funcionalidades de curriculum

--- modules/curriculum/controller.php
+++ modules/curriculum/controller.php
@@ -13,7 +13,6 @@ class CurriculumController {
 	function panel() {
     	SessionHandler()->check_session();
 		$curriculum_collection = Collector()->get('Curriculum');
-		print_r($curriculum_collection);exit;
 		$this->view->panel($curriculum_collection);
 	}
 }
--- modules/curriculum/view.php
+++ modules/curriculum/view.php
@@ -7,7 +7,7 @@ class CurriculumView extends View {
 		$gui = file_get_contents("static/modules/curriculum/panel.html");
 		$gui_tbl_curriculum = file_get_contents("static/modules/curriculum/tbl_curriculum.html");
 		$gui_tbl_curriculum = $this->render_regex('TBL_CURRICULUM', $gui_tbl_curriculum, $curriculum_collection);
-		
+		print_r($gui_tbl_curriculum);exit;
 		$render = str_replace('{tbl_curriculum}', $gui_tbl_curriculum, $gui);
 		$render = $this->render_breadcrumb($render);
 		$template = $this->render_template($render);
