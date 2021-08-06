22d105bca42faec884ba49c65f337a6b0e9d9337
Federico Vergara <hu.ce.ro@gmail.com>
2021-08-05 23:15:06 -0300

    Agrego funcionalidades de curriculum

--- modules/curriculum/controller.php
+++ modules/curriculum/controller.php
@@ -15,5 +15,15 @@ class CurriculumController {
 		$curriculum_collection = Collector()->get('Curriculum');
 		$this->view->panel($curriculum_collection);
 	}
+
+	function exportar_cv() {
+    	SessionHandler()->check_session();
+		require_once 'tools/exportar_cv.php';
+    	$fecha_desde = $_POST["fecha_desde"];
+		$fecha_hasta = $_POST["fecha_hasta"];
+		$query = file_get_contents("sql/exportar_cv.sql");
+		$array_data = array("{fecha_desde}"=>$fecha_desde, "{fecha_hasta}"=>$fecha_hasta);
+		ExportarReporte()->reporte_cv($query, $array_data);
+	}
 }
 ?>
