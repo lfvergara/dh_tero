9e9ae765db181f30b7b9c6b87850a4689c77ff26
Federico Vergara <hu.ce.ro@gmail.com>
2021-08-05 23:25:28 -0300

    Agrego funcionalidades de curriculum

--- modules/curriculum/controller.php
+++ modules/curriculum/controller.php
@@ -18,8 +18,9 @@ class CurriculumController {
 
 	function exportar_cv() {
     	SessionHandler()->check_session();
-		$fecha_desde = $_POST["fecha_desde"];
-		$fecha_hasta = $_POST["fecha_hasta"];
+    	require_once "tools/excelreport.php";
+		$fecha_desde = filter_input(INPUT_POST, "fecha_desde");
+		$fecha_hasta = filter_input(INPUT_POST, "fecha_hasta");
 		$select = "cv.denominacion AS DENOMINACION, cv.genero AS GENERO, cv.edad AS EDAD, cv.estudio AS ESTUDIO, cv.titulo AS TITULO, cv.estadocivil AS ESTADOCIVIL, cv.localidad AS LOCALIDAD, cv.direccion AS DIRECCION, cv.correo AS CORREO, cv.telefono AS TELEFONO, cv.mensaje AS MENSAJE, ai.denominacion AS AREA_INTERES, pr.denominacion AS PROVINCIA, cv.fecha AS FECHA, date_format('{fecha_desde}', '%y') as ANO, (SELECT count(cva.curriculum_id) FROM curriculum cva WHERE cva.fecha_carga BETWEEN '2019-01-01' AND '{fecha_hasta}') AS CONTADOR";
 		$from = "curriculum cv INNER JOIN areainteres ai ON cv.areainteres = ai.areainteres_id INNER JOIN provincia pr ON cv.provincia = pr.provincia_id";
 		$where = "cv.fecha_carga BETWEEN '{fecha_desde}' AND '{fecha_hasta}' ORDER BY cv.fecha_carga DESC";
