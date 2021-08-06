fa71e2aa159e818f8ca07998fcc6dbb9d58a326d
Federico Vergara <hu.ce.ro@gmail.com>
2021-08-05 23:37:03 -0300

    Agrego funcionalidades de curriculum

--- modules/curriculum/controller.php
+++ modules/curriculum/controller.php
@@ -18,7 +18,7 @@ class CurriculumController {
 
 	function exportar_cv() {
     	SessionHandler()->check_session();
-    	//require_once "tools/excelreport.php";
+    	require_once "tools/excelreport.php";
 		$fecha_desde = filter_input(INPUT_POST, "fecha_desde");
 		$fecha_hasta = filter_input(INPUT_POST, "fecha_hasta");
 		$select = "CONCAT(cv.apellido, ' ', cv.nombre) AS DENOMINACION, cv.estudio AS ESTUDIO, cv.titulo AS TITULO, cv.estadocivil AS ESTADOCIVIL, cv.localidad AS LOCALIDAD, cv.direccion AS DIRECCION, cv.correo AS CORREO, cv.telefono AS TELEFONO, cv.mensaje AS MENSAJE, ai.denominacion AS AREA_INTERES, pr.denominacion AS PROVINCIA, cv.fecha_carga AS FECHA, date_format('{$fecha_desde}', '%y') as ANO, (SELECT count(cva.curriculum_id) FROM curriculum cva WHERE cva.fecha_carga BETWEEN '2019-01-01' AND '{$fecha_hasta}') AS CONTADOR";
