d8c0202b47d8a792d7d5093f509775b46a7196db
Federico Vergara <hu.ce.ro@gmail.com>
2021-08-05 23:55:28 -0300

    Agrego funcionalidades de curriculum

--- tools/excelreport.php
+++ tools/excelreport.php
@@ -102,8 +102,8 @@ class ExcelReport extends View {
     header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
     header('Content-Disposition: attachment;filename="iinformeCV.xlsx"');
     header('Cache-Control: max-age=0');
-    print_r($array_exportacion);exit;
     $objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel2007');
+    print_r($objWriter);exit;
     file_put_contents('depuracion.txt', ob_get_contents());
     ob_end_clean();
     $objWriter->save('php://output');
