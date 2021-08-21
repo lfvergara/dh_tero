77a25ebc9ed8ab81d14bc8c0341a67a49397f04c
Federico Vergara <hu.ce.ro@gmail.com>
2021-08-21 11:35:53 -0300

    Agregando Banners

--- modules/banner/view.php
+++ modules/banner/view.php
@@ -6,7 +6,7 @@ class BannerView extends View {
 	function panel($banner_collection) {
 		$gui = file_get_contents("static/modules/banner/panel.html");
 		$gui_tbl_banner = file_get_contents("static/modules/banner/tbl_banner.html");
-		$gui_tbl_banner = $this->render_regex_dict('TBL_BANNER', $gui_tbl_banner, $banner_collection);
+		$gui_tbl_banner = $this->render_regex('TBL_BANNER', $gui_tbl_banner, $banner_collection);
 		$render = str_replace('{tbl_banner}', $gui_tbl_banner, $gui);
 		$render = $this->render_breadcrumb($render);
 		$template = $this->render_template($render);
@@ -16,7 +16,7 @@ class BannerView extends View {
 	function editar($obj_banner) {
 		$gui = file_get_contents("static/modules/banner/editar.html");
 		$gui_tbl_banner = file_get_contents("static/modules/banner/tbl_banner.html");
-		$gui_tbl_banner = $this->render_regex_dict('TBL_BANNER', $gui_tbl_banner, $banner_collection);
+		$gui_tbl_banner = $this->render_regex('TBL_BANNER', $gui_tbl_banner, $banner_collection);
 		$obj_banner = $this->set_dict($obj_banner);
 		$render = str_replace('{tbl_banner}', $gui_tbl_banner, $gui);
 		$render = $this->render($obj_banner, $render);
