4020cbfb11f2d8c9370926918d425f0b6fc13aad
Federico Vergara <hu.ce.ro@gmail.com>
2021-08-21 10:39:18 -0300

    Arreglando curriculum

--- core/helpers/appfiles.php
+++ core/helpers/appfiles.php
@@ -2,7 +2,7 @@
 $file = filter_input(INPUT_GET, "f");
  
 if(!is_null($file)) {
-	$archivo = URL_APPFILES . "{$file}";
+	$archivo = URL_PRIVATE . "{$file}";
 	if(file_exists($archivo)) {
 		$finfo = finfo_open(FILEINFO_MIME_TYPE);
 		$mime = finfo_file($finfo, $archivo);
