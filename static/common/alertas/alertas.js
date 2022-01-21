0d0a3ebb5930441bda69ec64e777b8659aed9f6f
Federico <hu.ce.ro@gmail.com>
2022-01-21 10:35:59 -0300

    Desde servidor

---
+++ static/common/alertas/alertas.js
@@ -0,0 +1,80 @@
+function ajax() {
+  var xmlhttp;
+  if (window.XMLHttpRequest){
+      xmlhttp=new XMLHttpRequest();
+  } else{
+      xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+  }
+  return xmlhttp;
+}
+
+var objAJAX = ajax();
+
+function confirma_formulario(tipo, path) {  
+  //var host = "http://"+window.location.hostname;
+  //var enlace = "/dh_tero/static/common/alertas/confirma_formulario.html";        
+  var enlace = "/static/common/alertas/confirma_formulario.html";    
+  var div_modal = document.getElementById('div_modal');
+  switch(tipo) {
+    case 1:
+      var msg = "¿Desea guardar el nuevo registro?";
+      break;
+    case 2:
+      var msg = "¿Desea confirmar la edición?";
+      break;
+    case 3:
+      var msg = "¿Desea editar el registro?";
+      break;
+    case 4:
+      var msg = "¿Desea eliminar el registro?";
+      break;
+    case 5:
+      var msg = "Está por AUTORIZAR la OP ¿Desea continuar?";
+      break;
+    case 6:
+      var msg = "Está por RECHAZAR la OP ¿Desea continuar?";
+      break;
+  }
+
+  objAJAX.onreadystatechange=function(){
+    if (objAJAX.readyState==4 && objAJAX.status==200){
+      var text = objAJAX.responseText;
+      text = text.replace('@msg@', msg);
+      div_modal.innerHTML = text;
+    }
+  }
+  
+  objAJAX.open("GET", enlace, false);
+  objAJAX.send(); 
+  $('#sistema_alertas').modal('show');
+  var btnConfirmar = document.getElementById('btnConfirmar');
+
+  switch(tipo) {
+    case 1:
+      btnConfirmar.onclick = function() { confirmar_formulario(); };
+      break;
+    case 2:
+      btnConfirmar.onclick = function() { confirmar_formulario(); };
+      break;
+    case 3:
+      btnConfirmar.onclick = function() { location.href = path; };
+      break;
+    case 4:
+      btnConfirmar.onclick = function() { location.href = path; };
+      break;
+    case 5:
+      btnConfirmar.onclick = function() { location.href = path; };
+      break;
+    case 6:
+      btnConfirmar.onclick = function() { location.href = path; };
+      break;
+  }
+}
+
+function confirmar_formulario() {
+  document.getElementById('formulario_guardar').submit();
+}
+
+function confirmar_enlace(path) {
+  location.href = path;
+}
