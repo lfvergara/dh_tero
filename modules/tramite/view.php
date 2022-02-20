739cf0048bacd82e1ad3398982d409421d5253ed
Federico Vergara <hu.ce.ro@gmail.com>
2022-02-20 00:17:55 -0300

    Empezamos nuevamente

--- modules/tramite/view.php
+++ modules/tramite/view.php
@@ -1,29 +1,29 @@
 <?php
 
 
-class DestinatarioView extends View {
+class TramiteView extends View {
 
-	function panel($destinatario_collection) {
-		$gui = file_get_contents("static/modules/destinatario/panel.html");
-		$gui_tbl_destinatario = file_get_contents("static/modules/destinatario/tbl_destinatario.html");
-		$gui_tbl_destinatario = $this->render_regex('TBL_DESTINATARIO', $gui_tbl_destinatario, $destinatario_collection);
-		$render = str_replace('{tbl_destinatario}', $gui_tbl_destinatario, $gui);
+	function panel($tramite_collection) {
+		$gui = file_get_contents("static/modules/tramite/panel.html");
+		$gui_tbl_tramite = file_get_contents("static/modules/tramite/tbl_tramite.html");
+		$gui_tbl_tramite = $this->render_regex('TBL_TRAMITE', $gui_tbl_tramite, $tramite_collection);
+		$render = str_replace('{tbl_tramite}', $gui_tbl_tramite, $gui);
 		$render = $this->render_breadcrumb($render);
 		$template = $this->render_template($render);
 		print $template;
 	}
 
-	function editar($destinatario_collection, $obj_destinatario) {
-		$gui = file_get_contents("static/modules/destinatario/editar.html");
-		$gui_tbl_destinatario = file_get_contents("static/modules/destinatario/tbl_destinatario.html");
-		$gui_tbl_destinatario = $this->render_regex('TBL_DESTINATARIO', $gui_tbl_destinatario, $destinatario_collection);
+	function editar($tramite_collection, $obj_tramite) {
+		$gui = file_get_contents("static/modules/tramite/editar.html");
+		$gui_tbl_tramite = file_get_contents("static/modules/tramite/tbl_tramite.html");
+		$gui_tbl_tramite = $this->render_regex('TBL_TRAMITE', $gui_tbl_tramite, $tramite_collection);
 
-		$obj_destinatario->checked_si = ($obj_destinatario->online == 0) ? '' : 'checked';
-		$obj_destinatario->checked_no = ($obj_destinatario->online == 0) ? 'checked' : '';
+		$obj_tramite->checked_si = ($obj_tramite->online == 0) ? '' : 'checked';
+		$obj_tramite->checked_no = ($obj_tramite->online == 0) ? 'checked' : '';
 
-		$obj_destinatario = $this->set_dict($obj_destinatario);
-		$render = str_replace('{tbl_destinatario}', $gui_tbl_destinatario, $gui);
-		$render = $this->render($obj_destinatario, $render);
+		$obj_tramite = $this->set_dict($obj_tramite);
+		$render = str_replace('{tbl_tramite}', $gui_tbl_tramite, $gui);
+		$render = $this->render($obj_tramite, $render);
 		$render = $this->render_breadcrumb($render);
 		$template = $this->render_template($render);
 		print $template;
